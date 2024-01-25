597d6cd719ab7f1c7f3854d9a1fb973951cdf7ea
Aidan_Varano <varanoab@merrimack.edu>
2024-01-25 11:54:45 -0500

    Comments

--- src/main/java/com/dessert_shoppe/DessertShoppe.java
+++ src/main/java/com/dessert_shoppe/DessertShoppe.java
@@ -10,12 +10,12 @@ import java.io.InputStreamReader;
 public class DessertShoppe {
     DessertShoppe(String shoppe_name, String inventory_file) {
         try (
-            // File file = new File(this.getClass().getResource("/res.txt").getFile());
             BufferedReader br = new BufferedReader(new InputStreamReader(
                            this.getClass().getResourceAsStream("/" + inventory_file)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
+                //add meaningfull processing to file
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,13 +24,14 @@ public class DessertShoppe {
 
     String placeOrder(User input_User, String order_path) {
         try (
-            // File file = new File(this.getClass().getResource("/res.txt").getFile());
 
             BufferedReader br = new BufferedReader(new InputStreamReader(
                            this.getClass().getResourceAsStream("/" + order_path)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
+                //add meaningfull processing to file
+
             }
         } catch (IOException e) {
             e.printStackTrace();
