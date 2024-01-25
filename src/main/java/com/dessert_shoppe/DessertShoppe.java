e2c24508397ad0e3aa517d905216a5a113dbe54e
Aidan_Varano <varanoab@merrimack.edu>
2024-01-25 12:21:42 -0500

    Note Comments

--- src/main/java/com/dessert_shoppe/DessertShoppe.java
+++ src/main/java/com/dessert_shoppe/DessertShoppe.java
@@ -6,16 +6,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+//need to make these use one function 
+
 
 public class DessertShoppe {
+
+    HashMap inventory;
+
     DessertShoppe(String shoppe_name, String inventory_file) {
         try (
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                           this.getClass().getResourceAsStream("/" + inventory_file)))) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        this.getClass().getResourceAsStream("/" + inventory_file)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                //add meaningfull processing to file
+                // add meaningfull processing to file
+                //Use first line to determine structure of hashmap 
+                //then create hashmap
+                //.put is used to add a key: inventory.put("Norway", "Oslo");
+                //is there  way to make this more efficient since we arleady know the number of items? 
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,13 +35,12 @@ public class DessertShoppe {
     String placeOrder(User input_User, String order_path) {
         try (
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                           this.getClass().getResourceAsStream("/" + order_path)))) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        this.getClass().getResourceAsStream("/" + order_path)))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                //add meaningfull processing to file
-
+                // add meaningfull processing to file
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,3 +50,34 @@ public class DessertShoppe {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //Google is informing me that a good load factor is .70 to .75, so I am thinking 1.3 times as large as the initial entry.Just round up if not an int
