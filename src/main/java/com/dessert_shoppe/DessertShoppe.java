a3a367e4d007a4bd50dc7952e441cf9bc8ba73a1
Aidan_Varano <varanoab@merrimack.edu>
2024-01-25 11:42:09 -0500

    Proper File Reading in DesserShoppe Class

--- src/main/java/com/dessert_shoppe/DessertShoppe.java
+++ src/main/java/com/dessert_shoppe/DessertShoppe.java
@@ -9,14 +9,24 @@ import java.io.InputStreamReader;
 
 public class DessertShoppe {
     DessertShoppe(String shoppe_name, String inventory_file) {
-
+        try (
+            // File file = new File(this.getClass().getResource("/res.txt").getFile());
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                           this.getClass().getResourceAsStream("/" + inventory_file)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     String placeOrder(User input_User, String order_path) {
         try (
             // File file = new File(this.getClass().getResource("/res.txt").getFile());
 
-                BufferedReader br = new BufferedReader(new InputStreamReader(
+            BufferedReader br = new BufferedReader(new InputStreamReader(
                            this.getClass().getResourceAsStream("/" + order_path)))) {
             String line;
             while ((line = br.readLine()) != null) {
