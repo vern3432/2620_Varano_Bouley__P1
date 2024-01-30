0fcd9e27c4ed6ffa30c44a7d5086ae8c90cf8b6b
Aidan_Varano <varanoab@merrimack.edu>
2024-01-30 12:06:59 -0500

    Basic and Sumewhat bad implimentation of Inventory proccesing

--- src/main/java/com/Item.java
+++ src/main/java/com/Item.java
@@ -1,5 +1,29 @@
 package com;
+//43, Sundae, Vanilla, 3.75, 3, Peanut Butter
 
 public class Item {
+    String name=null;
+    String typeOne=null;
+    double Price=0.0;
+    int quantity=0;
+    String typetwo=null;
+    
+
+
+    public Item(String name, String typeOne, double price, int quantity, String typetwo) {
+        this.name = name;
+        this.typeOne = typeOne;
+        Price = price;
+        this.quantity = quantity;
+        this.typetwo = typetwo;
+    }
+            
+
+
+
+    @Override
+    public String toString() {
+        return "Item []";
+    }
     
 }
--- src/main/java/com/dessert_shoppe/DessertShoppe.java
+++ src/main/java/com/dessert_shoppe/DessertShoppe.java
@@ -1,6 +1,7 @@
 package com.dessert_shoppe;
 
 import com.Item;
+import com.Item;
 import com.dessert_shoppe.User;
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,7 +9,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,11 +20,11 @@ import java.util.Map;
 public class DessertShoppe {
 
   String shoppe_name;
-  HashMap inventory;
+  HashMap<Integer, Item> inventory;
 
   DessertShoppe(String shoppe_name, String inventory_file) {
     this.shoppe_name = shoppe_name;
-    String proccessed = ProccessInv(inventory_file);
+    ProccessInv(inventory_file);
   }
 
   String placeOrder(User input_User, String order_path) {
@@ -36,22 +39,47 @@ public class DessertShoppe {
     return ("r");
   }
 
-  String ProccessInv(String order_path) {
-    ArrayList readInfo = fileProcess(order_path);
+  public void ProccessInv(String order_path) {
+    ArrayList<String> readInfo = fileProcess(order_path);
+    System.out.println("ProcessInv Arraylist created");
     // int size_map=Integer.parseInt(readInfo.get(0))/ 0.75 + 1;
-    //if key value exists, get the key i guess. 
-    Map<Integer, Item> map = new HashMap<>();
-    for (int i = 1; i < readInfo.size(); i++) {}
+    //if key value exists, get the key i guess.
+    //43, Sundae, Vanilla, 3.75, 3, Peanut Butter
+    HashMap<Integer, Item> InvMap = new HashMap<Integer, Item>();
+    System.out.println("ProcessInv HashMap created");
 
-    return ("r");
+    for (int i = 1; i < readInfo.size(); i++) {
+      List<String> temp = new LinkedList<String>(
+        Arrays.asList(readInfo.get(i).split(","))
+      );
+      while (temp.size() < 6) {
+        while ((temp.size() < 5)) {
+          temp.add("1");
+        }
+        temp.add(null);
+        System.out.println("added null to item temp");
+      }
+      System.out.println("adding" + temp.get(0));
+      Item tempItem = new Item(
+        temp.get(1),
+        temp.get(2),
+        Double.parseDouble(temp.get(3).replace(" ", "")),
+        Integer.parseInt(temp.get(4).replace(" ", "")),
+        temp.get(5)
+      );
+      InvMap.put(Integer.parseInt(temp.get(0)), tempItem);
+
+      System.out.println("added" + temp.get(0));
+    }
+    this.inventory = InvMap;
   }
 
-  public HashMap getInventory() {
+  public HashMap<Integer, Item> getInventory() {
     return inventory;
   }
 
   //read resource stream files and return lines as Arraylist
-  public ArrayList fileProcess(String filename) {
+  public ArrayList<String> fileProcess(String filename) {
     ArrayList<String> forReturn = new ArrayList<String>();
     try (
       BufferedReader br = new BufferedReader(
