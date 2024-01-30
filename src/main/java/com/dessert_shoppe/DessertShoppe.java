a4644a0324a166d960f6574021aafeb067bb9d0c
Aidan_Varano <varanoab@merrimack.edu>
2024-01-30 10:41:59 -0500

    Formatting

--- src/main/java/com/dessert_shoppe/DessertShoppe.java
+++ src/main/java/com/dessert_shoppe/DessertShoppe.java
@@ -12,81 +12,67 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//need to make these use one function 
-//has table size should always be prime, one less than a power of 2. 
+//need to make these use one function
+//has table size should always be prime, one less than a power of 2.
 public class DessertShoppe {
-    String shoppe_name;
-    HashMap inventory;
 
-    DessertShoppe(String shoppe_name, String inventory_file) {
-        this.shoppe_name=shoppe_name;
-        String proccessed = ProccessInv(inventory_file);
+  String shoppe_name;
+  HashMap inventory;
 
+  DessertShoppe(String shoppe_name, String inventory_file) {
+    this.shoppe_name = shoppe_name;
+    String proccessed = ProccessInv(inventory_file);
+  }
 
-    }
+  String placeOrder(User input_User, String order_path) {
+    String proccessed = ProccessOrder(order_path);
 
-    String placeOrder(User input_User, String order_path) {
-        String proccessed = ProccessOrder(order_path);
+    return ("reciept");
+  }
 
+  String ProccessOrder(String order_path) {
+    ArrayList<String> readInfo = fileProcess(order_path);
 
+    return ("r");
+  }
 
-        return ("reciept");
-    }
+  String ProccessInv(String order_path) {
+    ArrayList readInfo = fileProcess(order_path);
+    // int size_map=Integer.parseInt(readInfo.get(0))/ 0.75 + 1;
+    int size_map = 100;
+    Map<Integer, Item> map = new HashMap<>(size_map);
 
-    String ProccessOrder(String order_path) {
-        ArrayList<String> readInfo=fileProcess(order_path);
+    for (int i = 1; i < readInfo.size(); i++) {}
 
+    return ("r");
+  }
 
-        return ("r");
-    }
+  public HashMap getInventory() {
+    return inventory;
+  }
 
-    String ProccessInv(String order_path) {
-        ArrayList readInfo=fileProcess(order_path);
-        // int size_map=Integer.parseInt(readInfo.get(0))/ 0.75 + 1;
-        int size_map=100;
-        Map<Integer, Item> map = new HashMap<>(size_map);
+  //read resource stream files and return lines as Arraylist
+  public ArrayList fileProcess(String filename) {
+    ArrayList<String> forReturn = new ArrayList<String>();
+    try (
+      BufferedReader br = new BufferedReader(
+        new InputStreamReader(
+          this.getClass().getResourceAsStream("/" + filename)
+        )
+      )
+    ) {
+      String line;
 
-        for(int i=1;i<readInfo.size();i++){
-                
-
-        }
-
-
-        return ("r");
-    }
-
-    public HashMap getInventory() {
-        return inventory;
-    }
-
-
-
-
-
-    //read resource stream files and return lines as Arraylist
-    public ArrayList fileProcess(String filename){
-        ArrayList<String> forReturn=new ArrayList<String>();
-        try (
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                this.getClass().getResourceAsStream("/" + filename)))) {
-    String line;
-
-    while ((line = br.readLine()) != null) {
+      while ((line = br.readLine()) != null) {
         forReturn.add((line));
         // add meaningfull processing to file
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-        
-        return forReturn;
-        
+    return forReturn;
+  }
 }
-
-
-}
-
 // Google is informing me that a good load factor is .70 to .75, so I am
 // thinking 1.3 times as large as the initial entry.Just round up if not an int
--- src/main/java/com/dessert_shoppe/Main.java
+++ src/main/java/com/dessert_shoppe/Main.java
@@ -1,33 +1,35 @@
 //2620_Varano_Bouley__P1
 package com.dessert_shoppe;
 
-import java.io.FileNotFoundException;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    
-    public static void main(String[] args) throws FileNotFoundException {
-        // Create our new dessert shoppe, and give it an inventory to load
-        DessertShoppe shop = new DessertShoppe("OOPsy Daisy Desserts",
-                "testInventory.txt");
-        // Set up the three users in the system, including payment methods
-        User[] users = {
-                new User("Chris", new CreditCard(1000.0)),
-                new User("Zach", new GiftCard(100.0)),
-                new User("Sam", new CreditCard(1000.0))
-        };
-        // Build a receipt string by placing an order for each of the
-        // users (the order lives in the file)
-        String receipt = shop.placeOrder(users[0], "order1.txt");
-        receipt += "\n\n" + shop.placeOrder(users[1], "order2.txt");
-        receipt += "\n\n" + shop.placeOrder(users[2], "order3.txt");
-        // Print the receipt
-        System.out.println(receipt);
-        // Check the current remaining balance of each user
-        for (int i = 0; i < users.length; i++) {
-            System.out.println(users[i]);
-        }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    // Create our new dessert shoppe, and give it an inventory to load
+    DessertShoppe shop = new DessertShoppe(
+      "OOPsy Daisy Desserts",
+      "testInventory.txt"
+    );
+    // Set up the three users in the system, including payment methods
+    User[] users = {
+      new User("Chris", new CreditCard(1000.0)),
+      new User("Zach", new GiftCard(100.0)),
+      new User("Sam", new CreditCard(1000.0)),
+    };
+    // Build a receipt string by placing an order for each of the
+    // users (the order lives in the file)
+    String receipt = shop.placeOrder(users[0], "order1.txt");
+    receipt += "\n\n" + shop.placeOrder(users[1], "order2.txt");
+    receipt += "\n\n" + shop.placeOrder(users[2], "order3.txt");
+    // Print the receipt
+    System.out.println(receipt);
+    // Check the current remaining balance of each user
+    for (int i = 0; i < users.length; i++) {
+      System.out.println(users[i]);
     }
+  }
 }
